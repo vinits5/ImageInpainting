f60240668bd5bfe4fb5e82d03ce2aad2e3c98427
root <vinitsarode5@gmail.com>
2021-04-06 10:12:40 +0000

    location to store log is changed

--- main.py
+++ main.py
@@ -294,7 +294,7 @@ def test():
     csv = pd.DataFrame(losses)
     csv.to_csv(f"{result_dir}/losses.csv")
 
-    cmd = f"gsutil -m cp -r {result_dir} gs://vinit_helper/cloth_inpainting_gan/cloth_inpainting_eccv20_aim/{opt.checkpoints_dir.split('/')[1]}"
+    cmd = f"gsutil -m cp -r {result_dir} gs://vinit_helper/cloth_inpainting_gan/cloth_inpainting_local_binary_pattern/{opt.checkpoints_dir.split('/')[1]}"
     os.system(cmd)  
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -76,10 +76,10 @@ class BaseModel():
                     torch.save(net.cpu().state_dict(), save_path)
                 
                 if which_epoch>1:
-                    cmd = f"gsutil -m cp -r {save_path} gs://vinit_helper/cloth_inpainting_gan/cloth_inpainting_eccv20_aim/{self.save_dir.split('/')[1]}"
+                    cmd = f"gsutil -m cp -r {save_path} gs://vinit_helper/cloth_inpainting_gan/cloth_inpainting_local_binary_pattern/{self.save_dir.split('/')[1]}"
                     os.system(cmd)   
         if which_epoch == 1:
-            cmd = f"gsutil -m cp -r {self.save_dir} gs://vinit_helper/cloth_inpainting_gan/cloth_inpainting_eccv20_aim/"
+            cmd = f"gsutil -m cp -r {self.save_dir} gs://vinit_helper/cloth_inpainting_gan/cloth_inpainting_local_binary_pattern/"
             os.system(cmd)
 
     def __patch_instance_norm_state_dict(self, state_dict, module, keys, i=0):
